ec990e1f2167e5e920800926474c7d9c0c9a4847
Kai Deng <kd18h@my.fsu.edu>
2020-12-29 13:58:54 -0500

    commit by Kai

--- R/TEMM_utility.R
+++ R/TEMM_utility.R
@@ -46,7 +46,7 @@ mu_err = function(Mu,mu_est,K,idx,id_est){
 
 
 mkronecker = function(X){
-  #X is a list of matrix to do kronecker product
+  #X is a list of matrices for kronecker product
 
   M = length(X)
   KronX = X[[M]]
@@ -60,10 +60,75 @@ mkronecker = function(X){
 }
 
 
+krondet = function(X,log=TRUE){
+  #X is a list of matrices
+  #Calculate the determinant of Kronecker product of X
+  
+  M = length(X)
+  dimen = sapply(X, ncol)
+  p = prod(dimen)
+  
+  logdet = log(sapply(X, det))
+  mydet = p*sum(logdet/dimen)
+  
+  if(log){
+    return(mydet)
+  }
+  else{
+    return(exp(mydet))
+  }
+}
+
+
+
+tensrloglk = function(X, espi, Mu, SIG){
+  #calculate observed loglikelihood
+  #X is a list of observed tensor data
+  #espi is estimate weight
+  #Mu is a list of estimate cluster mean, of length K
+  #SIG is a list of estimate covariance matrices, of length M
+  
+  n = length(X)
+  dimen = dim(X[[1]])
+  M = length(dimen)
+  p = prod(dimen)
+  K = length(Mu)
+  
+  SIGinv = lapply(SIG, MASS::ginv)
+  logSIGdet = krondet(SIG,log=TRUE)
+  
+  B = array(list(),K-1)
+  for (k in 2:K) {
+    muk_1 = rTensor::as.tensor(Mu[[k]]-Mu[[1]])
+    muk1 = rTensor::as.tensor(Mu[[k]]+Mu[[1]])
+    B[[k-1]] = rTensor::ttl(muk_1, SIGinv, ms=1:M)
+  }
+  
+  loglk = 0
+  for (i in 1:n){
+    X_mu1 = rTensor::as.tensor(X[[i]]-Mu[[1]])
+    temp_mu1 = rTensor::ttl(X_mu1, SIGinv, ms=1:M)
+    dis_mu1 =  TRES::ttt(X_mu1, temp_mu1, 1:M)@data
+    
+    logf1 = -p*log(2*pi)/2 - logSIGdet/2 - dis_mu1/2
+    
+    for (k in 2:K){
+      temp = espi[1]
+      logfkoverf1 = TRES::ttt(B[[k-1]], rTensor::as.tensor(X[[i]]), 1:M)@data -
+        TRES::ttt(B[[k-1]], muk1, 1:M)@data/2
+      fkoverf1 = exp(logfkoverf1)
+      temp = temp + espi[k]*fkoverf1
+    }
+    loglk = loglk+log(temp)+logf1
+  }
+  
+  return(loglk)
+}
+
 
 logMixTenGau = function(Xm, pi, eta, Mu, SIG){
   #calculate observed loglikelihood
-  #Xm is observed tensor data
+  #Each column of Xm is a vectorized observed tensor data
   #pi is estimate weight
   #eta is estimate probability of Xi belong to class k
   #Mu is a list of estimate cluster mean, of length K
